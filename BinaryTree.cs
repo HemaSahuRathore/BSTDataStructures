a72f7a1379a1ca77c99eaf963165dd03d9ddc529
Hema <hemasahu019@gmail.com>
2022-08-10 15:22:26 +0530

    Added UC1 code to create BT

---
+++ BinaryTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D15BinaryTree
+{
+    public class BinaryTree<T> where T : IComparable<T>
+    {
+        public T nodeValue;
+        public BinaryTree<T> leftTree;
+        public BinaryTree<T> rightTree;
+
+        public BinaryTree(T nodeValue) //constructor
+        {
+            this.nodeValue = nodeValue;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+
+        //method to insert element
+        public void Insert(T value)
+        {
+            T currentNodeValue = nodeValue;
+            if(currentNodeValue.CompareTo(value) > 0)
+            {
+                if (this.leftTree == null)
+                    this.leftTree = new BinaryTree<T>(value);
+                else
+                    this.leftTree.Insert(value);
+            }
+            else
+            {
+                if(this.rightTree == null)
+                    this.rightTree = new BinaryTree<T>(value);
+                else
+                    this.rightTree.Insert(value);
+            }
+
+        }    
+        
+        //display
+        public void Display()
+        {  
+            if(this.leftTree != null)
+            {
+                this.leftTree.Display();    
+            }
+            Console.WriteLine(this.nodeValue.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightTree.Display();   
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Binary Tree Data Structure");
+            Console.WriteLine("Welcome to Binary Tree Data Structure"); //welcome msg
+
+            BinaryTree<int> binaryTree = new BinaryTree<int>(56);
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Insert(85);
+            binaryTree.Insert(23);
+            binaryTree.Insert(81);
+            binaryTree.Insert(96);
+            binaryTree.Display();
         }
     }
 }
