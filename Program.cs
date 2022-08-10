70ad7d7358ab3ea1102898f294d405986e756583
Hema <hemasahu019@gmail.com>
2022-08-10 13:14:07 +0530

    Added welcome message code to main

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D15BinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Tree Data Structure");
+        }
+    }
+}
