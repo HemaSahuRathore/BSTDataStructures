aee5ccfc0910b7746bbc5ca448fe3411f70f0b68
Hema <hemasahu019@gmail.com>
2022-08-10 15:26:40 +0530

    Added UC1 code to create BT

--- Program.cs
+++ Program.cs
@@ -9,10 +9,6 @@
             BinaryTree<int> binaryTree = new BinaryTree<int>(56);
             binaryTree.Insert(30);
             binaryTree.Insert(70);
-            binaryTree.Insert(85);
-            binaryTree.Insert(23);
-            binaryTree.Insert(81);
-            binaryTree.Insert(96);
             binaryTree.Display();
         }
     }
